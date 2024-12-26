62bbfd9c8055e1216b32f60f7f6c9ec15120d1e3
chelseaaaaapin <chelseariyantashila06@gmail.com>
2024-12-26 08:11:19 +0700

    Add files via upload

---
+++ MatematikaBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika mtk = new Matematika();
+        mtk.pertambahan(90, 90);
+        mtk.pengurangan(100, 30);
+        mtk.perkalian(20, 20);
+        mtk.pembagian(200, 2);
+    }
+    
+}
